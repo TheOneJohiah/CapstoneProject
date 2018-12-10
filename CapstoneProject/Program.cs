3dfc0f5d9f4d59ee1a5dcfc0b4665e57c0f99d1f
Micah Thoreson <thoresonm1@gmail.com>
2018-12-09 23:00:32 -0500

    Almost finished the menu, all that is left is final neatening and the game cycle itself.

--- CapstoneProject/Program.cs
+++ CapstoneProject/Program.cs
@@ -47,6 +47,7 @@ namespace CapstoneProject
 
         private static void ExecuteCommand(string menuAnswer, string player1Name, string player2Name, string player1Letter, string player2Letter, string victor, int startPlayer, string[] places)
         {
+            HelperMethods.DisplayHeader("Main menu");
             switch (menuAnswer)
             {
                 case "a":
@@ -56,15 +57,29 @@ namespace CapstoneProject
                     player2Name = ChoosePlayerName(player2Name);
                     break;
                 case "c":
-                    player1Letter = ChoosePlayerSymbol(player1Letter);
-                    if (player1Letter == player2Letter)
+                    player1Letter = ChoosePlayerSymbol(player1Letter, player1Name);
+                    player2Letter = ChoosePlayerSymbol(player2Letter, player2Name);
+                    while (player1Letter == player2Letter)
                     {
-                        Console.WriteLine($"You will need to change {player2Letter}'s letter as well to play.");
-                        player2Letter = ChoosePlayerSymbol(player2Letter);
+                        Console.WriteLine("Your letters cannot be identical! Please enter new letters.");
+                        player1Letter = ChoosePlayerSymbol(player1Letter, player1Name, player2Letter);
+                        player2Letter = ChoosePlayerSymbol(player2Letter, player2Name, player1Letter);
                     }
                     break;
                 case "d":
-                    
+                    startPlayer = 1;
+                    Console.WriteLine($"The starting player is {player1Name}");
+                    HelperMethods.DisplayContinuePrompt();
+                    break;
+                case "e":
+                    startPlayer = 2;
+                    Console.WriteLine($"The starting player is {player2Name}");
+                    HelperMethods.DisplayContinuePrompt();
+                    break;
+                case "f":
+                    startPlayer = ChooseStartingPlayer(startPlayer, player1Name, player2Name);
+                    break;
+                case "q":
                     break;
                 default:
                     Console.WriteLine($"Sorry, {menuAnswer} is not a valid answer. Please choose only an option listed on the menu.");
@@ -74,10 +89,81 @@ namespace CapstoneProject
 
         }
 
+        private static string ChoosePlayerSymbol(string playerLetter, string playerName, string otherPlayerLetter)
+        {
+            bool validLetter = false;
+            do
+            {
+                HelperMethods.DisplayHeader($"Choose X or O for {playerName}");
+                Console.WriteLine($"\nThe other player has {otherPlayerLetter}");
+                playerLetter = Console.ReadLine().ToUpper();
+                if (playerLetter != "X" && playerLetter != "O")
+                {
+                    Console.WriteLine($"{playerLetter} is not a valid letter! Please try again.");
+                    HelperMethods.DisplayContinuePrompt();
+                }
+                else
+                {
+                    validLetter = true;
+                }
+            } while (validLetter == false);
+            Console.WriteLine($"Your letter is {playerLetter}");
+            HelperMethods.DisplayContinuePrompt();
+            return playerLetter;
+        }
+
+        private static void DisplayRules()
+        {
+            HelperMethods.DisplayHeader("The rules of Tic Tac Toe");
+
+            Console.WriteLine("\nTic Tac Toe is a fairly simple game.");
+            Console.WriteLine("The two players take turns filling the grid with their respective letter,");
+            Console.WriteLine("and the first to get three in a row wins.");
+            Console.WriteLine("If the grid fills without three in a row, there is no winner.");
+
+            HelperMethods.DisplayContinuePrompt();
+        }
+
+        private static int ChooseStartingPlayer(int startPlayer, string player1Name, string player2Name)
+        {
+            Random rand = new Random();
+
+            HelperMethods.DisplayHeader("Randomize the starting player");
+            HelperMethods.DisplayContinuePrompt();
+            Console.WriteLine("\nRandomizing...");
+            System.Threading.Thread.Sleep(1000);
+            startPlayer = rand.Next(1, 3);
+            if (startPlayer == 1)
+            {
+                Console.WriteLine($"The starting player is {player1Name}");
+            }
+            else
+            {
+                Console.WriteLine($"The starting player is {player2Name}");
+            }
+            HelperMethods.DisplayContinuePrompt();
+            return startPlayer;
+        }
+
         private static string ChoosePlayerSymbol(string playerLetter, string playerName)
         {
-            HelperMethods.DisplayHeader($"Choose a new letter for {playerName}");
-            playerLetter = Console.ReadLine();
+            bool validLetter = false;
+            do
+            {
+                HelperMethods.DisplayHeader($"Choose X or O for {playerName}");
+                playerLetter = Console.ReadLine().ToUpper();
+                if (playerLetter != "X" && playerLetter != "O")
+                {
+                    Console.WriteLine($"{playerLetter} is not a valid letter! Please try again.");
+                    HelperMethods.DisplayContinuePrompt();
+                }
+                else
+                {
+                    validLetter = true;
+                }
+            } while (validLetter == false);
+            Console.WriteLine($"Your letter is {playerLetter}");
+            HelperMethods.DisplayContinuePrompt();
             return playerLetter;
         }
 
@@ -97,7 +183,10 @@ namespace CapstoneProject
             Console.WriteLine("A) Choose player 1 name");
             Console.WriteLine("B) Choose player 2 name");
             Console.WriteLine("C) Choose X or O");
-            Console.WriteLine("D) Choose starting player");
+            Console.WriteLine("D) Give player 1 first move");
+            Console.WriteLine("E) Give player 2 first move");
+            Console.WriteLine("F) Randomise first move");
+            Console.WriteLine("G) View Tic Tac Toe rules");
             Console.WriteLine("Q) Quit");
             menuAnswer = Console.ReadLine().ToLower();
 
